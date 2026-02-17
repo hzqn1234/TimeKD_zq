82c94513a16a129de43ff2b08c5939ae3e8368d4
Hu Zongqi <zongqi001@e.ntu.edu.sg>
2026-02-17 15:47:11 +0800

    CAI with teacher

--- model/CAI_model.py
+++ model/CAI_model.py
@@ -6,27 +6,22 @@ import math
 # from utils import args
 
 class Amodel(nn.Module):
-    def __init__(self, series_dim, feature_dim, target_num, hidden_num, hidden_dim, drop_rate=0.5, use_series_oof=False, device='cuda'):
+    def __init__(self,  series_dim, feature_dim, target_num, hidden_num, hidden_dim,
+                        d_llm=768,
+                        drop_rate=0.5, use_series_oof=False, device='cuda'
+                        ):
         super(Amodel, self).__init__()
         self.device=device
         hidden_feature_dropout = 0.01
         self.use_series_oof = use_series_oof
         self.input_series_block_n1 = nn.Sequential(
                                         nn.Linear(series_dim, hidden_dim),
-                                        # nn.Linear(13, hidden_dim),
                                         nn.LayerNorm(hidden_dim)
                                         )
-        # self.input_series_block_n2 = nn.Sequential(
-        #                                 nn.Linear(series_dim, hidden_dim)
-        #                                 ,nn.LayerNorm(hidden_dim)
-        #                                 )
-        # self.input_feature_block = nn.Sequential(
-        #                                 nn.Linear(feature_dim, hidden_dim)
-        #                                 ,nn.BatchNorm1d(hidden_dim)
-        #                                 ,nn.Dropout(hidden_feature_dropout)
-        #                                 ,nn.LeakyReLU()
-        #                                 )
-        # self.gru_series = nn.GRU(hidden_dim, hidden_dim, batch_first=True, bidirectional=True)
+        self.input_series_block_n1_t = nn.Sequential(
+                                        nn.Linear(d_llm, hidden_dim),
+                                        nn.LayerNorm(hidden_dim)
+                                        )
         encoder_layer = nn.TransformerEncoderLayer(
                                                     d_model         = hidden_dim, 
                                                     nhead           = 16, 
@@ -35,27 +30,21 @@ class Amodel(nn.Module):
                                                     # dropout         = 0.1
                                                     )
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=3)
-        
-        # decoder_layer = nn.TransformerDecoderLayer(
-        #                                             d_model         = hidden_dim, 
-        #                                             nhead           = 2, 
-        #                                             dim_feedforward = 128, 
-        #                                             dropout         = 0.1
-        #                                             )
-        # self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=2)
-        
-        # self.hidden_feature_block = []
-        # for h in range(hidden_num-1):
-        #     self.hidden_feature_block.extend([
-        #                              nn.Linear(hidden_dim, hidden_dim)
-        #                              ,nn.BatchNorm1d(hidden_dim)
-        #                              ,nn.Dropout(hidden_feature_dropout)
-        #                              # ,nn.Dropout(drop_rate)
-        #                              ,nn.LeakyReLU()
-        #                              ])
-        # self.hidden_feature_block = nn.Sequential(*self.hidden_feature_block)
+        self.transformer_encoder_t = nn.TransformerEncoder(encoder_layer, num_layers=3)
 
         self.output_block = nn.Sequential(
+                                         nn.BatchNorm1d(1*hidden_dim)
+                                         ,nn.Linear(1*hidden_dim, 1*hidden_dim)
+                                         ,nn.Dropout(0.05)
+                                         ,nn.LeakyReLU()
+
+                                         ,nn.Linear(1*hidden_dim, 1*hidden_dim)
+                                         ,nn.LeakyReLU()
+                                         
+                                         ,nn.Linear(1*hidden_dim, target_num)
+                                         ,nn.Sigmoid()
+                                         )
+        self.output_block_t = nn.Sequential(
                                          nn.BatchNorm1d(1*hidden_dim)
                                          ,nn.Linear(1*hidden_dim, 1*hidden_dim)
                                          ,nn.Dropout(0.05)
@@ -70,26 +59,6 @@ class Amodel(nn.Module):
 
     def param_num(self):
         return sum([param.nelement() for param in self.parameters()])
-
-    # def batch_gru(self,series,mask):
-    #     node_num = mask.sum(dim=-1).detach().cpu()
-    #     pack = nn.utils.rnn.pack_padded_sequence(series, node_num, batch_first=True, enforce_sorted=False)
-    #     message,hidden = self.gru_series(pack)
-    #     pooling_feature = []
-
-    #     for i,n in enumerate(node_num.numpy()):
-    #         n = int(n)
-    #         bi = 0
-
-    #         si = message.unsorted_indices[i]
-    #         for k in range(n):
-
-    #             if k == n-1:
-    #                 sample_feature = message.data[bi+si]
-    #             bi = bi + message.batch_sizes[k]
-
-    #         pooling_feature.append(sample_feature)
-    #     return torch.stack(pooling_feature,0)
 
     def transformer_pooling(self, transfomer_message, mask):
         node_num = mask.sum(dim=-1).detach().cpu().tolist()
@@ -110,22 +79,31 @@ class Amodel(nn.Module):
         ## TSF
         # print(x_series.shape)
         # torch.Size([128, 13, 223])
+        if data['batch_emb_tensor'] is None:
+            x1_tsf_enc = self.input_series_block_n1(x_series) # [128, 13, 223]
+            x1_tsf_enc = x1_tsf_enc.permute(1, 0, 2) # [13,128,256]
+            x1_tsf     = self.transformer_encoder(x1_tsf_enc) # [13,128,256]
+            x1_tsf_pool = self.transformer_pooling(x1_tsf, mask) # [128,256]
+            y = self.output_block(x1_tsf_pool).squeeze(1)
 
-        x1_tsf_enc = self.input_series_block_n1(x_series)
-        x1_tsf_enc = x1_tsf_enc.permute(1, 0, 2) # [13,128,256]
-        x1_tsf     = self.transformer_encoder(x1_tsf_enc) # [13,128,256]
-        # x1_tsf_pool = x1_tsf.mean(dim=0)
-        x1_tsf_pool = self.transformer_pooling(x1_tsf, mask) # [128,256]
-        
-        # ## GRU
-        # x1_gru = self.input_series_block_n2(x_series)
-        # x1_gru = self.batch_gru(x1_gru,mask)
-        
-        ## Concat
-        # x1 = torch.cat([x1_tsf,x1_gru],axis=1)
-        x1 = x1_tsf_pool
+            x1_tsf_enc_t = None
+            y_t = None
+            x1_tsf_pool_t = None
+        else:
+            ## student
+            x1_tsf_enc = self.input_series_block_n1(x_series)
+            x1_tsf_enc = x1_tsf_enc.permute(1, 0, 2) # [13,128,256]
+            x1_tsf     = self.transformer_encoder(x1_tsf_enc) # [13,128,256]
+            x1_tsf_pool = self.transformer_pooling(x1_tsf, mask) # [128,256]
+            y = self.output_block(x1_tsf_pool).squeeze(1)            
 
-        y = self.output_block(x1).squeeze(1)
-        return x1_tsf_enc, None, y, None, x1_tsf_pool, None
+            ## teacher
+            x_emb = data['batch_emb_tensor'].to(self.device)
+            x1_tsf_enc_t = self.input_series_block_n1_t(x_emb) # [128, 13, 768]
+            x1_tsf_enc_t = x1_tsf_enc_t.permute(1, 0, 2) # [13,128,256]
+            x1_tsf_t     = self.transformer_encoder_t(x1_tsf_enc_t) # [13,128,256]
+            x1_tsf_pool_t = self.transformer_pooling(x1_tsf_t, mask) # [128,256]
+            y_t = self.output_block_t(x1_tsf_pool_t).squeeze(1)
+        return x1_tsf_enc, x1_tsf_enc_t, y, y_t, x1_tsf_pool, x1_tsf_pool_t
         # return ts_enc, prompt_enc, ts_out, prompt_out, ts_att_pool, prompt_att_avg
         # ts_out, ts_enc, ts_att_avg
